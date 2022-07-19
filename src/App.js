0ef923367c55f8589aa0150a46cd226efc9b07f1
Victor Patru <patruvictor.mihai@gmail.com>
2022-07-19 11:35:05 +0300

    feat: basic navbar component

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { BrowserRouter as Route, Route } from "react-router-dom";
+import { BrowserRouter as Router, Route } from "react-router-dom";
 import Navbar from "./components/layout/Navbar";
 
 function App() {
