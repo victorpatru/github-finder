0d2894fb19b2d45ab4061492f249378bc2cdcfe1
Victor Patru <patruvictor.mihai@gmail.com>
2022-07-20 12:42:43 +0300

    feat: add loading spinner animation

--- src/components/users/UserResults.jsx
+++ src/components/users/UserResults.jsx
@@ -1,4 +1,5 @@
 import { useEffect, useState } from "react";
+import Spinner from "../layout/Spinner";
 
 function UserResults() {
 
@@ -42,7 +43,7 @@ function UserResults() {
             </div>
         )
     } else {
-        return <h3>Loading...</h3>
+        return <Spinner />
     }
 
 
