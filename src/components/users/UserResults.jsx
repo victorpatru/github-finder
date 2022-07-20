9e820c429181945ad904dc32b578a4d95b38fbb6
Victor Patru <patruvictor.mihai@gmail.com>
2022-07-20 12:58:23 +0300

    feat: display github users

--- src/components/users/UserResults.jsx
+++ src/components/users/UserResults.jsx
@@ -1,5 +1,6 @@
 import { useEffect, useState } from "react";
 import Spinner from "../layout/Spinner";
+import UserItem from "./UserItem";
 
 function UserResults() {
 
@@ -37,8 +38,8 @@ function UserResults() {
             // Setting up a grid layout that changes depending on the screen size
             <div className="grid grid-cols-1 gap-8 xl:grid-cols-4 lg:grid-cols-3 md:gird-cols-2">
                 {/* Mapping over our users state and getting the github names of our individuals */}
-                {users.map((user, i) => (
-                    <h3 key={i}>{user.login}</h3>
+                {users.map(user => (
+                    <UserItem key={user.id} user={user} />
                 ))}
             </div>
         )
